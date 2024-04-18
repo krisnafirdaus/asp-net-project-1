b49bfb0e3378b185ca4457bb3317b7cf397250c1
Krisna Firdaus <krisnafirdaus@gmail.com>
2024-04-18 22:01:11 +0700

    add new test simple

---
+++ krisna-dto/Controllers/SimpleData.cs
@@ -0,0 +1,33 @@
+﻿using krisna_dto.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace krisna_dto.Controllers
+{
+    [Route("api/[controller]")]
+    public class SimpleData : ControllerBase
+    {
+        [HttpGet("GetData")]
+        public IActionResult GetData()
+        {
+            var result = new SimpleDataInputDTO
+            {
+                Name = "krisna",
+                Date = "10-20-2024"
+            };
+
+            return Ok(result);
+        }
+
+        [HttpPost("PostData")]
+        public IActionResult PostData([FromBody] SimpleDataInputDTO simpleDataInput)
+        {
+            var result = new SimpleDataInputDTO
+            {
+                Name = simpleDataInput.Name,
+                Date = simpleDataInput.Date
+            };
+
+            return Ok(result);
+        }
+    }
+}
---
+++ krisna-dto/DTOs/SimpleDataInputDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace krisna_dto.DTOs
+{
+	public class SimpleDataInputDTO
+	{
+        public string Name { get; set; }
+        public string Date { get; set; }
+    }
+}
+
